f73f823eed09993961cc2c47a7ef20c9952c9a27
tgvinodkumar <tgvinodkumar@gmail.com>
2020-05-09 14:26:35 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Gautam Bhagat Thimmapuram', // Required
+  description: '2nd Grader',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
